1ca49e661abb2099cef31a72dddc65f5d38486b3
ZaredMaldon <zaredosvaldom@gmail.com>
2022-11-28 21:55:16 -0600

    estructura

--- app/src/main/java/com/example/sistemasmoviles/ActivityLogin.kt
+++ app/src/main/java/com/example/sistemasmoviles/ActivityLogin.kt
@@ -2,10 +2,20 @@ package com.example.sistemasmoviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+
 
 class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+    }
+
+    fun onClickAbReg(view: View) { //cambiar a pantalla de registro
+        val change = Intent(this,ActivityRegister::class.java)
+        startActivity(change)
     }
 }
--- app/src/main/java/com/example/sistemasmoviles/ActivityRegister.kt
+++ app/src/main/java/com/example/sistemasmoviles/ActivityRegister.kt
@@ -2,10 +2,15 @@ package com.example.sistemasmoviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+
+    fun onClick(view: View) {//Registro
+
     }
 }
